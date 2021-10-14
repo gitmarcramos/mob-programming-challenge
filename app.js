c32e6a6fa64ebceacb6196ac6644b9b78e3aabe8
Marc Ramos <59534439+gitmarcramos@users.noreply.github.com>
2021-10-14 12:04:34 +0200

    iteration 6

--- app.js
+++ app.js
@@ -1,37 +1,67 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
-require('dotenv').config();
+require("dotenv").config();
 
-require('./config/mongodb.js');
-const hbs = require('hbs');
+require("./config/mongodb.js");
+const hbs = require("hbs");
 
-app.use(express.static(__dirname + './public'))
+app.use(express.static(__dirname + "/public"));
 
 const users = [
-    {
-        name: "Marc",
-        email: "marcomarc@marc.marc",
-        favoriteLanguage: "php"
-    },
+  {
+    name: "Marc",
+    email: "marcomarc@marc.marc",
+    favoriteLanguage: "php",
+  },
 
-    {
-        name: "Joey",
-        email: "joey@gmail.com",
-        favoriteLanguage: "python"
-    },
+  {
+    name: "Joey",
+    email: "joey@gmail.com",
+    favoriteLanguage: "python",
+  },
 
-    {
-        name: "Priscilla Gasztowtt",
-        email: "priscilla@gasztow.tt",
-        favoriteLanguage: "js ofc"
-    }
+  {
+    name: "Priscilla Gasztowtt",
+    email: "priscilla@gasztow.tt",
+    favoriteLanguage: "js ofc",
+  },
+  {
+    name: "Pauline",
+    email: "pauline@pauline.pauline",
+    favoriteLanguage: "portuguese",
+  },
 ];
 
 const images = [
-    "/img/dolphin.jpg",
-    "/img/hacker.jpg",
-    "/img/penguin.jpg"
-]
+  "/img/dolphin.jpg",
+  "/img/hacker.jpg",
+  "/img/happyppl.jpg",
+  "/img/penguin.jpg",
+];
 
-app.listen(process.env.PORT);
+// Route for home sweet home :)
+app.get(["/", "home"], (req, res, next) => {
+  res.render("home.hbs", {
+    images,
+  });
+});
+
+// Route for the squad
+app.get("/my-dev-squad", (req, res, next) => {
+  res.render("my-dev-squad.hbs", {
+    users,
+  });
+});
+
+// route to add happy people
+app.get("/add-new-ironhacker", (req, res, next) => {
+  res.render("add-new-ironhacker.hbs", {
+      
+  });
+});
+
+// Kickstart
+app.listen(process.env.PORT, () => {
+  console.log("App running on port: http://localhost:" + process.env.PORT);
+});
